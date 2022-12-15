afd9e04a9496bfc94b9a3ff5f5dd80cd9bc1b793
Rubayet-billah <rubayetbillah909@gmail.com>
2022-12-16 04:23:40 +0600

    checking

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <h1 className='text-6xl font-mono font-bold mt-48'>Welcome everyone from team WebArtist</h1>
-      <h1 className='text-6xl font-mono font-bold'>This is from dashboard branch.</h1>
     </div>
   );
 }
