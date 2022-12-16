cfa10d164e496e08007f43e3056c66269a28dd9a
Rubayet-billah <rubayetbillah909@gmail.com>
2022-12-17 00:28:45 +0600

    components setup

---
+++ src/Pages/Dashboard/Departments/Deparment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Deparment = () => {
+    return (
+        <div>
+            <h1>This is from department</h1>
+        </div>
+    );
+};
+
+export default Deparment;
---
+++ src/Pages/Dashboard/Employees/ViewEmployee.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ViewEmployee = () => {
+    return (
+        <div>
+            <h1>this is from view ViewEmployee</h1>
+        </div>
+    );
+};
+
+export default ViewEmployee;
