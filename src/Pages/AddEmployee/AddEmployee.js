226757bba0666a38634683fd0dbdbdd0be126367
Rubayet-billah <rubayetbillah909@gmail.com>
2022-12-17 15:41:50 +0600

    components stucture changed

---
+++ src/Pages/AddEmployee/AddEmployee.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddEmployee = () => {
+    return (
+        <div>
+            This change is from Rubayet.
+        </div>
+    );
+};
+
+export default AddEmployee;
