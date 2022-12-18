d9e6ae24276f96a32929f11a4f0cbcc2eda9d5d3
Naem-hossain <naem.hossaint@gmail.com>
2022-12-18 18:37:29 +0600

    add banner folder

---
+++ src/Pages/Homepage/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import img from '../Banner/Banner'
+
+const Banner = () => {
+    return (
+        <div>
+            <img src='https://www.blueoceanrecruitment.com/wp-content/uploads/2021/08/hr-manager-jobs-in-dubai-abu-dhabi-sharjah-uae-800x550.png'
+                className='w-full h-96'
+                alt="" />
+            <h1>My name is nayeem</h1>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Homepage/Home/Home.js
+++ src/Pages/Homepage/Home/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
+
             <h1 className="text-6xl font-mono font-bold mt-48">
                 Welcome everyone from team WebArtist
-            </h1>
 
+            </h1>
             <h2>Hello Everyone</h2>
+            <Banner></Banner>
         </div>
     );
 };
