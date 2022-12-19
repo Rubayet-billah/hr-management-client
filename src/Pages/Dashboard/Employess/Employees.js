a45190fcb2f7e679e22520ac2a5acc6da013eb00
itarek99 <tarekulislam7060@gmail.com>
2022-12-20 01:31:27 +0600

    my commit

--- src/Pages/Dashboard/Employess/Employees.js
+++ src/Pages/Dashboard/Employess/Employees.js
@@ -7,7 +7,7 @@ const Employees = () => {
       .then((res) => res.json())
       .then((data) => setEmployees(data));
   }, []);
-  // tarek changed this line 1:26
+  // tarek changed this line 1:31
   return (
     <div>
       <div class='overflow-x-auto relative shadow-md sm:rounded-lg'>
