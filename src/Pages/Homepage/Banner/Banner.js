e88bf2be05b9d594531d5e6a3a7e791627c63f94
Rubayet-billah <rubayetbillah909@gmail.com>
2022-12-19 20:53:19 +0600

    json changed

--- src/Pages/Homepage/Banner/Banner.js
+++ src/Pages/Homepage/Banner/Banner.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import img from '../Banner/Banner'
+import img from '../../../assets/Banner/bannerhr.png'
 
 const Banner = () => {
     return (
         <div>
-            <img src='https://www.blueoceanrecruitment.com/wp-content/uploads/2021/08/hr-manager-jobs-in-dubai-abu-dhabi-sharjah-uae-800x550.png'
+            <img src={img}
                 className='w-full'
                 alt="" />
-            <h1>My name is nayeem</h1>
         </div>
     );
 };
