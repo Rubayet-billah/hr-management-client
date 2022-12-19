8c94db30f6a037b0559d87efedcaa78d8d10f875
Rubayet-billah <rubayetbillah909@gmail.com>
2022-12-20 01:23:25 +0600

    route setup

---
+++ src/Pages/Dashboard/Admins/Admins.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Admins = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Admins;
--- src/Routes/Routes/routes.js
+++ src/Routes/Routes/routes.js
@@ -3,38 +3,53 @@ import DashboardLayout from "../../Layouts/DashboardLayout/DashboardLayout";
 import MainlayOut from "../../Layouts/MainLayout/MainlayOut";
 import Login from "../../Pages/Authentication/Login/Login";
 import Register from "../../Pages/Authentication/Register/Register";
+import Admins from "../../Pages/Dashboard/Admins/Admins";
+import Candidates from "../../Pages/Dashboard/Candidates/Candidates";
 import Dashboard from "../../Pages/Dashboard/Dashboard";
+import Employees from "../../Pages/Dashboard/Employess/Employees";
 import Home from "../../Pages/Homepage/Home/Home";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainlayOut></MainlayOut>,
-    children: [
-      {
+    {
         path: "/",
-        element: <Home></Home>,
-      },
-      {
-        path: "/login",
-        element: <Login></Login>,
-      },
-      {
-        path: "/register",
-        element: <Register></Register>,
-      },
-    ],
-  },
-  {
-    path: "/dashboard",
-    element: <DashboardLayout></DashboardLayout>,
-    children: [
-      {
+        element: <MainlayOut></MainlayOut>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>,
+            },
+            {
+                path: "/login",
+                element: <Login></Login>,
+            },
+            {
+                path: "/register",
+                element: <Register></Register>,
+            },
+        ],
+    },
+    {
         path: "/dashboard",
-        element: <Dashboard></Dashboard>,
-      },
-    ],
-  },
+        element: <DashboardLayout></DashboardLayout>,
+        children: [
+            {
+                path: "/dashboard",
+                element: <Dashboard></Dashboard>,
+            },
+            {
+                path: "/dashboard/employees",
+                element: <Employees></Employees>,
+            },
+            {
+                path: "/dashboard/admins",
+                element: <Admins></Admins>,
+            },
+            {
+                path: "/dashboard/candidates",
+                element: <Candidates></Candidates>,
+            },
+        ],
+    },
 ]);
 
 export default router;
