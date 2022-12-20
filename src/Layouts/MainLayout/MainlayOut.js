49ca674cf160bfd98d49be7648a3662a8d3fc463
Rubayet-billah <rubayetbillah909@gmail.com>
2022-12-20 19:40:37 +0600

    footer added

--- src/Layouts/MainLayout/MainlayOut.js
+++ src/Layouts/MainLayout/MainlayOut.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../../Shared/Footer/Footer';
 import Navbar from '../../Shared/Navbar/Navbar';
 
 const MainlayOut = () => {
@@ -7,6 +8,7 @@ const MainlayOut = () => {
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Pages/Homepage/HiringBanner/HiringBanner.js
+++ src/Pages/Homepage/HiringBanner/HiringBanner.js
@@ -3,11 +3,11 @@ import hiringImg from '../../../assets/hiringImage/hiring.webp'
 
 const HiringBanner = () => {
     return (
-        <section className='lg:flex'>
+        <section className='lg:flex my-8'>
             <div className='lg:w-1/2'><img className='w-full' src={hiringImg} alt="" /></div>
-            <div className='flex items-center lg:pl-12 lg:w-1/2'>
+            <div className='flex items-center justify-center lg:justify-start lg:pl-12 lg:w-1/2'>
                 <div>
-                    <h1 className='text-3xl md:text-5xl font-bold text-rose-500'>Come On and Join Us</h1>
+                    <h1 className='text-3xl md:text-5xl font-bold text-rose-500 my-6'>Come On and Join Us</h1>
                     <h3 className='text-xl md:text-3xl'>Requirements:</h3>
                     <li>Lorem ipsum dolor sit.</li>
                     <li>Quae possimus blanditiis odit!</li>
