63bef5381575b63d8dca1cb9e4a899013a94b09a
Rubayet-billah <rubayetbillah909@gmail.com>
2022-12-23 01:28:14 +0600

    candidate table added

--- src/Pages/Dashboard/Candidates/Candidates.js
+++ src/Pages/Dashboard/Candidates/Candidates.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import { Table } from 'flowbite-react';
+import React, { useState } from 'react';
 
 const Candidates = () => {
+    const [candidates, setCandidates] = useState([]);
+
+
     return (
         <div>
-
+            <Table striped={true}>
+                <Table.Head>
+                    <Table.HeadCell>
+                        Applicant's Name
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Designation
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Expected Salary
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Experience
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        <span className="sr-only">
+                            Edit
+                        </span>
+                    </Table.HeadCell>
+                </Table.Head>
+                <Table.Body className="divide-y">
+                    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                        <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                            Apple MacBook Pro 17"
+                        </Table.Cell>
+                        <Table.Cell>
+                            Sliver
+                        </Table.Cell>
+                        <Table.Cell>
+                            Laptop
+                        </Table.Cell>
+                        <Table.Cell>
+                            $2999
+                        </Table.Cell>
+                        <Table.Cell>
+                            <a
+                                href="/tables"
+                                className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                            >
+                                Edit
+                            </a>
+                        </Table.Cell>
+                    </Table.Row>
+                </Table.Body>
+            </Table>
         </div>
     );
 };
