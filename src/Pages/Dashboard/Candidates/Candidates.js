f795351885328149198b914ddfd5f9fca134a720
Rubayet-billah <rubayetbillah909@gmail.com>
2022-12-23 02:11:47 +0600

    candidate info added

--- src/Pages/Dashboard/Candidates/Candidates.js
+++ src/Pages/Dashboard/Candidates/Candidates.js
@@ -3,11 +3,14 @@ import React, { useEffect, useState } from 'react';
 
 const Candidates = () => {
     const [candidates, setCandidates] = useState([]);
+    console.log(candidates)
+
 
     useEffect(() => {
-        fetch('candidates.json')
+        fetch('/candidates.json')
             .then(res => res.json())
             .then(data => {
+                console.log(data)
                 setCandidates(data)
             })
     }, [])
@@ -22,10 +25,13 @@ const Candidates = () => {
                         Designation
                     </Table.HeadCell>
                     <Table.HeadCell>
+                        Address
+                    </Table.HeadCell>
+                    <Table.HeadCell>
                         Expected Salary
                     </Table.HeadCell>
                     <Table.HeadCell>
-                        Experience
+                        Experience (y)
                     </Table.HeadCell>
                     <Table.HeadCell>
                         <span className="sr-only">
@@ -35,31 +41,37 @@ const Candidates = () => {
                 </Table.Head>
                 <Table.Body className="divide-y">
                     {
-                        candidates?.map()
+                        candidates?.map(candidate => <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                <div className='flex items-center'>
+                                    <img class="w-10 h-10 rounded-full" src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="Rounded avatar" />
+                                    <span className='ml-2'>{candidate.name}</span>
+                                </div>
+                            </Table.Cell>
+                            <Table.Cell>
+                                {candidate.designation}
+                            </Table.Cell>
+                            <Table.Cell>
+                                {candidate.address}
+                            </Table.Cell>
+                            <Table.Cell>
+                                ${candidate.expectedSalary}
+                            </Table.Cell>
+                            <Table.Cell>
+                                {candidate.experience}
+                            </Table.Cell>
+                            <Table.Cell>
+                                <a
+                                    href="/tables"
+                                    className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                                >
+                                    Edit
+                                </a>
+                            </Table.Cell>
+                        </Table.Row>)
                     }
 
-                    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                        <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                            Apple MacBook Pro 17"
-                        </Table.Cell>
-                        <Table.Cell>
-                            Sliver
-                        </Table.Cell>
-                        <Table.Cell>
-                            Laptop
-                        </Table.Cell>
-                        <Table.Cell>
-                            $2999
-                        </Table.Cell>
-                        <Table.Cell>
-                            <a
-                                href="/tables"
-                                className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                            >
-                                Edit
-                            </a>
-                        </Table.Cell>
-                    </Table.Row>
+
                 </Table.Body>
             </Table>
         </div>
