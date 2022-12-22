c94bd33ccf805567ad68c6b81f788a78abe362f0
mickeymaruf <mickeymaruf@gmail.com>
2022-12-22 17:23:13 +0600

    drawer icons tooltips added

--- src/Layouts/DashboardLayout/DashboardLayout.js
+++ src/Layouts/DashboardLayout/DashboardLayout.js
@@ -7,6 +7,7 @@ import { FiUsers } from 'react-icons/fi';
 import { ImUserTie } from 'react-icons/im';
 import { FcDepartment } from 'react-icons/fc';
 import { RiAdminLine } from 'react-icons/ri';
+import { Tooltip } from 'flowbite-react';
 
 const DashboardLayout = () => {
     const [open, setOpen] = useState(false);
@@ -16,53 +17,103 @@ const DashboardLayout = () => {
                 <div className={`absolute cursor-pointer right-0 translate-x-1/2 top-9 p-2 border-2 bg-red-700 text-white rounded-full ${open && 'rotate-180'} duration-300`}
                     onClick={() => setOpen(!open)}
                 >
-                    <SlArrowRight className='w-5 h-5' />
+                    <SlArrowRight className='w-3 h-3' />
                 </div>
                 <div className='p-5 flex flex-col justify-between h-full'>
                     <div>
-                        <div className={`flex items-center gap-3 ${open ? 'mt-4' : 'mt-1'} duration-200`}>
-                            <img className='w-10' src="https://img.icons8.com/color/48/null/dashboard-layout.png" alt='' />
-                            <h1 className={`origin-left ${!open && 'scale-0'} duration-200 text-2xl font-medium`}>Staff Deck</h1>
-                        </div>
+                        <Link to="/dashboard">
+                            <div className={`flex items-center gap-3 ${open ? 'mt-4' : 'mt-1'} duration-100`}>
+                                <img className='w-10' src="https://img.icons8.com/color/48/null/dashboard-layout.png" alt='' />
+                                <h1 className={`origin-left ${!open && 'scale-0'} duration-100 text-2xl font-medium`}>Staff Deck</h1>
+                            </div>
+                        </Link>
                         <div className='mt-8'>
-                            <Link to="/dashboard" className='flex items-center gap-3 hover:bg-gray-200 rounded-lg mb-3'>
-                                <div className={`${!open ? 'p-2' : 'p-3'}`}>
-                                    <AiOutlineDashboard className='w-5 h-5' />
-                                </div>
-                                <h1 className={`origin-left ${!open && 'hidden'} duration-200 font-medium`}>Dashboard</h1>
+                            <Link to="/dashboard" className='flex items-center gap-3 hover:bg-gray-200 rounded-lg mb-3 p-2'>
+                                {
+                                    open
+                                        ?
+                                        <AiOutlineDashboard className={`w-5 h-5`} />
+                                        : <Tooltip
+                                            content="Dashboard"
+                                            placement="right"
+                                            style="light"
+                                        >
+
+                                            <AiOutlineDashboard className={`w-5 h-5`} />
+                                        </Tooltip>
+                                }
+                                <h1 className={`origin-left ${!open && 'scale-0'} duration-200 font-medium`}>Dashboard</h1>
                             </Link>
-                            <Link to="/dashboard/candidates" className='flex items-center gap-3 hover:bg-gray-200 rounded-lg mb-3'>
-                                <div className={`${!open ? 'p-2' : 'p-3'}`}>
-                                    <FiUsers className='w-5 h-5' />
-                                </div>
-                                <h1 className={`origin-left ${!open && 'hidden'} duration-200 font-medium`}>Users</h1>
+                            <Link to="/dashboard/candidates" className='flex items-center gap-3 hover:bg-gray-200 rounded-lg mb-3 p-2'>
+                                {
+                                    open
+                                        ?
+                                        <FiUsers className={`w-5 h-5`} />
+                                        : <Tooltip
+                                            content="Candidates"
+                                            placement="right"
+                                            style="light"
+                                        >
+
+                                            <FiUsers className={`w-5 h-5`} />
+                                        </Tooltip>
+                                }
+                                <h1 className={`origin-left ${!open && 'scale-0'} duration-200 font-medium`}>Users</h1>
                             </Link>
-                            <Link to="/dashboard/departments" className='flex items-center gap-3 hover:bg-gray-200 rounded-lg mb-3'>
-                                <div className={`${!open ? 'p-2' : 'p-3'}`}>
-                                    <FcDepartment className='w-5 h-5' />
-                                </div>
-                                <h1 className={`origin-left ${!open && 'hidden'} duration-200 font-medium`}>Department</h1>
+                            <Link to="/dashboard/departments" className='flex items-center gap-3 hover:bg-gray-200 rounded-lg mb-3 p-2'>
+                                {
+                                    open
+                                        ?
+                                        <FcDepartment className={`w-5 h-5`} />
+                                        : <Tooltip
+                                            content="Departments"
+                                            placement="right"
+                                            style="light"
+                                        >
+
+                                            <FcDepartment className={`w-5 h-5`} />
+                                        </Tooltip>
+                                }
+                                <h1 className={`origin-left ${!open && 'scale-0'} duration-200 font-medium`}>Department</h1>
                             </Link>
-                            <Link to="/dashboard/employees" className='flex items-center gap-3 hover:bg-gray-200 rounded-lg mb-3'>
-                                <div className={`${!open ? 'p-2' : 'p-3'}`}>
-                                    <ImUserTie className='w-5 h-5 text-sky-500' />
-                                </div>
-                                <h1 className={`origin-left ${!open && 'hidden'} duration-200 font-medium`}>Employees</h1>
+                            <Link to="/dashboard/employees" className='flex items-center gap-3 hover:bg-gray-200 rounded-lg mb-3 p-2'>
+                                {
+                                    open
+                                        ?
+                                        <ImUserTie className={`w-5 h-5 text-sky-500`} />
+                                        : <Tooltip
+                                            content="Employees"
+                                            placement="right"
+                                            style="light"
+                                        >
+
+                                            <ImUserTie className={`w-5 h-5 text-sky-500`} />
+                                        </Tooltip>
+                                }
+                                <h1 className={`origin-left ${!open && 'scale-0'} duration-200 font-medium`}>Employees</h1>
                             </Link>
-                            <Link to="/dashboard/admins" className='flex items-center gap-3 hover:bg-gray-200 rounded-lg mb-3'>
-                                <div className={`${!open ? 'p-2' : 'p-3'}`}>
-                                    <RiAdminLine className='w-5 h-5 text-green-500' />
-                                </div>
-                                <h1 className={`origin-left ${!open && 'hidden'} duration-200 font-medium`}>Admins</h1>
+                            <Link to="/dashboard/admins" className='flex items-center gap-3 hover:bg-gray-200 rounded-lg mb-3 p-2'>
+                                {
+                                    open
+                                        ?
+                                        <RiAdminLine className={`w-5 h-5 text-green-500`} />
+                                        : <Tooltip
+                                            content="Admins"
+                                            placement="right"
+                                            style="light"
+                                        >
+
+                                            <RiAdminLine className={`w-5 h-5 text-green-500`} />
+                                        </Tooltip>
+                                }
+                                <h1 className={`origin-left ${!open && 'scale-0'} duration-200 font-medium`}>Admins</h1>
                             </Link>
                         </div>
                     </div>
                     <div>
                         <Link className='flex items-center gap-3'>
-                            <div>
-                                <img className='w-9' src="https://w7.pngwing.com/pngs/481/915/png-transparent-computer-icons-user-avatar-woman-avatar-computer-business-conversation-thumbnail.png" alt="" />
-                            </div>
-                            <h1 className={`origin-left ${!open && 'hidden'} duration-200 text-xs font-medium`}>Md Maruf Hossain</h1>
+                            <img className='w-9' src="https://w7.pngwing.com/pngs/481/915/png-transparent-computer-icons-user-avatar-woman-avatar-computer-business-conversation-thumbnail.png" alt="" />
+                            <h1 className={`origin-left ${!open && 'scale-0'} duration-200 text-xs font-medium`}>Md Maruf Hossain</h1>
                         </Link>
 
                     </div>
