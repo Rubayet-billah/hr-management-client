9eb3b8f6c858288243e0332bf57b0e1950766a7a
itarek99 <tarekulislam7060@gmail.com>
2022-12-22 17:54:56 +0600

    minor fixes

--- src/Pages/Dashboard/Employess/Components/EmployeeRow.js
+++ src/Pages/Dashboard/Employess/Components/EmployeeRow.js
@@ -17,45 +17,43 @@ const EmployeeRow = ({
   };
 
   return (
-    <>
-      <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>
-        <th scope='row' className='flex items-center py-4 px-6 text-gray-900 whitespace-nowrap dark:text-white'>
-          <img className='w-10 h-10 rounded-full' src={image} alt={fname} />
-          <div className='pl-3'>
-            <div className='text-base font-semibold'>
-              {fname} {lname}
-            </div>
-            <div className='font-normal text-gray-500'>{email}</div>
+    <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>
+      <th scope='row' className='flex items-center py-4 px-6 text-gray-900 whitespace-nowrap dark:text-white'>
+        <img className='w-10 h-10 rounded-full' src={image} alt={fname} />
+        <div className='pl-3'>
+          <div className='text-base font-semibold'>
+            {fname} {lname}
           </div>
-        </th>
-        <td className='py-4 px-6'>{phone}</td>
-        <td className='py-4 px-6'>
-          <div>
-            <p className='text-base text-gray-700'>Web Developer</p>
-            <p>{dept}</p>
-          </div>
-        </td>
-        <td className='py-4 px-6'>10 Jan, 2023</td>
-        <td className='py-4 px-6'>
-          <div className='flex'>
-            <button className='text-lg mr-2'>
-              <MdPreview
-                onClick={() => {
-                  setViewModalVisibility(true);
-                  setViewEmployee(employee);
-                }}
-              />
-            </button>
-            <button className='text-lg mr-2'>
-              <BiEdit />
-            </button>
-            <button onClick={() => deleteModalOpen(employee)} className='text-lg text-red-600'>
-              <FiDelete />
-            </button>
-          </div>
-        </td>
-      </tr>
-    </>
+          <div className='font-normal text-gray-500'>{email}</div>
+        </div>
+      </th>
+      <td className='py-4 px-6'>{phone}</td>
+      <td className='py-4 px-6'>
+        <div>
+          <p className='text-base text-gray-700'>Web Developer</p>
+          <p>{dept}</p>
+        </div>
+      </td>
+      <td className='py-4 px-6'>10 Jan, 2023</td>
+      <td className='py-4 px-6'>
+        <div className='flex'>
+          <button className='text-lg mr-2'>
+            <MdPreview
+              onClick={() => {
+                setViewModalVisibility(true);
+                setViewEmployee(employee);
+              }}
+            />
+          </button>
+          <button className='text-lg mr-2'>
+            <BiEdit />
+          </button>
+          <button onClick={() => deleteModalOpen(employee)} className='text-lg text-red-600'>
+            <FiDelete />
+          </button>
+        </div>
+      </td>
+    </tr>
   );
 };
 export default EmployeeRow;
