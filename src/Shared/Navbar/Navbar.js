25fd7b30ef757fed5d44f6692d0b4193ce1fbfe4
itarek99 <tarekulislam7060@gmail.com>
2022-12-22 18:31:17 +0600

    navbar update

--- src/Shared/Navbar/Navbar.js
+++ src/Shared/Navbar/Navbar.js
@@ -1,45 +1,34 @@
 import { Navbar } from 'flowbite-react';
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NAvbar = () => {
-    return (
-        <div className='text-center'>
-            <Navbar
-                fluid={true}
-                rounded={true}
-            >
-                <Navbar.Brand>
-                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-                        HR Manager
-                    </span>
-                </Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar.Collapse>
-                    <Navbar.Link
-                        href="/"
-                        active={true}
-                    >
-                        Home
-                    </Navbar.Link>
-                    <Navbar.Link href="/dashboard">
-                        Dashboard
-                    </Navbar.Link>
-                    <Navbar.Link href="/">
-                        About
-                    </Navbar.Link>
-                    <Navbar.Link href="/">
-                        Services
-                    </Navbar.Link>
-                    <Navbar.Link href="/login">
-                        Login
-                    </Navbar.Link>
-                    <Navbar.Link href="/register">
-                        Register
-                    </Navbar.Link>
-                </Navbar.Collapse>
-            </Navbar>
-        </div>
-    );
+  return (
+    <Navbar className='!px-4 md:!px-7 !py-5' fluid={true} rounded={true}>
+      <Navbar.Brand>
+        <span className='self-center whitespace-nowrap text-2xl font-semibold dark:text-white'>HR Manager</span>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className='!items-center'>
+        <Navbar.Link>
+          <Link to='/'>Home</Link>
+        </Navbar.Link>
+
+        <Navbar.Link>
+          <Link to='/dashboard'>Dashboard</Link>
+        </Navbar.Link>
+        <Navbar.Link href='/'>
+          <Link to='/login'>About</Link>
+        </Navbar.Link>
+        <Navbar.Link href='/'>
+          <Link to='/login'>Services</Link>
+        </Navbar.Link>
+
+        <Navbar.Link href='/login'>
+          <Link to='/login'>Login</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default NAvbar;
