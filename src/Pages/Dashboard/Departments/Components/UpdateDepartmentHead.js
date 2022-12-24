43f274f8c2a3151a7ce156029c4f4ce05aa70113
Naem-hossain <naem.hossaint@gmail.com>
2022-12-24 20:12:07 +0600

    added update head section

--- src/Pages/Dashboard/Departments/Components/UpdateDepartmentHead.js
+++ src/Pages/Dashboard/Departments/Components/UpdateDepartmentHead.js
@@ -1,24 +1,29 @@
-import { Button, Label, Modal, Select, TextInput } from 'flowbite-react';
+import { Button, Label, Modal, TextInput } from 'flowbite-react';
 import React from 'react';
 import { toast } from 'react-hot-toast';
 
-const AddDepartment = ({ addUserModalVisibility, setAddUserModalVisibility }) => {
-    const addModalClose = () => {
-        setAddUserModalVisibility(false);
+const UpdateDepartmentHead = ({
+    setAddDepartmentHeadModalVisibility,
+    addDepartmentHeadModalVisibility }) => {
+
+
+
+    const addDepartmentHeadModalClose = () => {
+        setAddDepartmentHeadModalVisibility(false);
     };
-    const handleAddDepartment = (e) => {
+
+    const handleUpdateDepartmentHead = (e) => {
         e.preventDefault()
-        toast.success('Department Added Successfully ')
+        toast.success('Department head added Successfully ')
     }
 
     return (
-
-        <Modal show={addUserModalVisibility} size='5xl' popup={true} onClose={addModalClose}>
+        <Modal show={addDepartmentHeadModalVisibility} size='xl' className='text-center' popup={true} onClose={addDepartmentHeadModalClose}>
             <Modal.Header />
             <Modal.Body>
                 <div className='px-6 pb-4 sm:pb-6 lg:px-8 xl:pb-8'>
                     <h3 className='text-xl mb-4 font-medium text-gray-900 dark:text-white'>Add New Department</h3>
-                    <form onSubmit={handleAddDepartment}>
+                    <form onSubmit={handleUpdateDepartmentHead}>
                         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4'>
 
                             <div>
@@ -27,17 +32,11 @@ const AddDepartment = ({ addUserModalVisibility, setAddUserModalVisibility }) =>
                                 </div>
                                 <TextInput id='firstName' placeholder='Department name' required={true} />
                             </div>
-                            <div>
-                                <div className='mb-2 block'>
-                                    <Label htmlFor='department' value='Department Head' />
-                                </div>
-                                <TextInput id='firstName' placeholder='Department Head' required={true} />
-                            </div>
                         </div>
                         <div className='w-full mt-4 '>
                             <Button type='submit'
 
-                            >Add Department</Button>
+                            >Update Department  Head</Button>
                         </div>
                     </form>
                 </div>
@@ -46,4 +45,4 @@ const AddDepartment = ({ addUserModalVisibility, setAddUserModalVisibility }) =>
     );
 };
 
-export default AddDepartment;
+export default UpdateDepartmentHead;
