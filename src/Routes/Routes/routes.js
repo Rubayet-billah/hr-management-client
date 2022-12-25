491af1011b0381e0d7fe385f841b185ffee193fb
rajibdas-it <rajibrad@gmail.com>
2022-12-25 12:21:06 +0600

    create payroll table

--- src/Routes/Routes/routes.js
+++ src/Routes/Routes/routes.js
@@ -9,62 +9,67 @@ import Dashboard from "../../Pages/Dashboard/Dashboard";
 import Departments from "../../Pages/Dashboard/Departments/Components/Departments";
 import Employees from "../../Pages/Dashboard/Employess/Employees";
 import Holidays from "../../Pages/Dashboard/Holidays/Holidays";
+import Payroll from "../../Pages/Dashboard/Payroll/Payroll";
 import Home from "../../Pages/Homepage/Home/Home";
 import Profile from "../../Pages/Profile/Profile";
 
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <MainlayOut></MainlayOut>,
+    children: [
+      {
         path: "/",
-        element: <MainlayOut></MainlayOut>,
-        children: [
-            {
-                path: "/",
-                element: <Home></Home>,
-            },
-            {
-                path: "/login",
-                element: <Login></Login>,
-            },
-            {
-                path: "/register",
-                element: <Register></Register>,
-            },
-        ],
-    },
-    {
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout></DashboardLayout>,
+    children: [
+      {
         path: "/dashboard",
-        element: <DashboardLayout></DashboardLayout>,
-        children: [
-            {
-                path: "/dashboard",
-                element: <Dashboard></Dashboard>,
-            },
-            {
-                path: "/dashboard/employees",
-                element: <Employees></Employees>,
-            },
-            {
-                path: "/dashboard/admins",
-                element: <Admins></Admins>,
-            },
-            {
-                path: "/dashboard/candidates",
-                element: <Candidates></Candidates>,
-            },
-            {
-                path: "/dashboard/departments",
-                element: <Departments></Departments>,
-            },
-            {
-                path: "/dashboard/profile",
-                element: <Profile></Profile>
-            },
-            {
-                path: "/dashboard/holidays",
-                element: <Holidays></Holidays>,
-            },
-        ],
-    },
+        element: <Dashboard></Dashboard>,
+      },
+      {
+        path: "/dashboard/employees",
+        element: <Employees></Employees>,
+      },
+      {
+        path: "/dashboard/payroll",
+        element: <Payroll></Payroll>,
+      },
+      {
+        path: "/dashboard/admins",
+        element: <Admins></Admins>,
+      },
+      {
+        path: "/dashboard/candidates",
+        element: <Candidates></Candidates>,
+      },
+      {
+        path: "/dashboard/departments",
+        element: <Departments></Departments>,
+      },
+      {
+        path: "/dashboard/profile",
+        element: <Profile></Profile>,
+      },
+      {
+        path: "/dashboard/holidays",
+        element: <Holidays></Holidays>,
+      },
+    ],
+  },
 ]);
 
 export default router;
