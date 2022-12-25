5766e6316d41276e78699f66c16ca460f22d7e0b
itarek99 <tarekulislam7060@gmail.com>
2022-12-25 22:14:19 +0600

    testimonial section added

---
+++ src/Pages/Homepage/Components/Testimonial.js
@@ -0,0 +1,11 @@
+const Testimonial = () => {
+  return (
+    <div className='container mx-auto px-2 py-12'>
+      <div className='text-center mb-24'>
+        <h3 className='text-4xl font-bold mb-1'>What Our User Say About Us</h3>
+        <p className='capitalize'>Some Of Our User Reviews.</p>
+      </div>
+    </div>
+  );
+};
+export default Testimonial;
