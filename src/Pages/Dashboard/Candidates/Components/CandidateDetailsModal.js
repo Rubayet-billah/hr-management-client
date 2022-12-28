733e4fe768fc16ef70a01181867dea5b9b60fca2
Rubayet-billah <rubayetbillah909@gmail.com>
2022-12-29 02:45:05 +0600

    candidate details modal edited

--- src/Pages/Dashboard/Candidates/Components/CandidateDetailsModal.js
+++ src/Pages/Dashboard/Candidates/Components/CandidateDetailsModal.js
@@ -69,7 +69,7 @@ const CandidateDetailsModal = ({
                                             </span>
                                             <div className="my-4 grid md:grid-cols-2 gap-x-5 gap-y-3 lg:mt-6 w-full">
                                                 <p className='flex items-center'><MdEmail className='mr-2 text-xl' /> {email}</p>
-                                                <p className='flex items-center'><MdPhone className='mr-2 text-xl' /> {phone}</p>
+                                                <p className='flex items-center'><MdPhone className='mr-2 text-xl' /> {phone || '01784589564'}</p>
                                                 <p className='flex items-center'><IoLocation className='mr-2 md:mr-1 text-xl md:-translate-x-[2px]' /> {address}</p>
                                                 <p className='flex items-center'><AiFillTool className='mr-2 text-xl' /> {experience}+ years</p>
                                                 <div className='md:col-span-2'><span className='font-medium'>Skills</span> <br />
