f5b14009bcc3d4ecc4284642b7ffc52dab73d030
itarek99 <tarekulislam7060@gmail.com>
2022-12-28 21:52:28 +0600

    employee search workin

--- src/Pages/Dashboard/Employess/Components/EmployeeStatistics.js
+++ src/Pages/Dashboard/Employess/Components/EmployeeStatistics.js
@@ -4,27 +4,29 @@ const EmployeeStatistics = ({ employees }) => {
   const maleEmployees = employees.filter((employee) => employee.gender === 'male');
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-3 gap-8 mb-8'>
-      <div className='bg-white py-6 px-10 rounded-lg flex items-center justify-between'>
+    <div className='grid grid-cols-3 gap-4 md:gap-8 mb-8'>
+      <div className='bg-white py-3 md:py-6 px-4 md:px-10 rounded-lg flex items-center justify-between'>
         <div>
-          <p className='text-gray-500'>Total Employee</p>
+          <p className='text-gray-500'>
+            Total <span className='hidden md:inline'>Employee</span>
+          </p>
           <p className='text-3xl font-medium text-gray-700'>{employees.length}</p>
         </div>
-        <BiBarChart className='text-5xl text-gray-700' />
+        <BiBarChart className='text-5xl text-gray-700 hidden md:block' />
       </div>
-      <div className='bg-white py-6 px-10 rounded-lg flex items-center justify-between'>
+      <div className='bg-white py-3 md:py-6 px-4 md:px-10 rounded-lg flex items-center justify-between'>
         <div>
           <p className='text-gray-500'>Male</p>
           <p className='text-3xl font-medium text-gray-700'>{maleEmployees.length}</p>
         </div>
-        <BiBarChart className='text-5xl text-gray-700' />
+        <BiBarChart className='text-5xl text-gray-700 hidden md:block' />
       </div>
-      <div className='bg-white py-6 px-10 rounded-lg flex items-center justify-between'>
+      <div className='bg-white py-3 md:py-6 px-4 md:px-10 rounded-lg flex items-center justify-between'>
         <div>
           <p className='text-gray-500'>Female</p>
           <p className='text-3xl font-medium text-gray-700'>{employees.length - maleEmployees.length}</p>
         </div>
-        <BiBarChart className='text-5xl text-gray-700' />
+        <BiBarChart className='text-5xl text-gray-700 hidden md:block' />
       </div>
     </div>
   );
--- src/Pages/Dashboard/Employess/Employees.js
+++ src/Pages/Dashboard/Employess/Employees.js
@@ -5,6 +5,7 @@ import EmployeeStatistics from './Components/EmployeeStatistics';
 import { AiOutlinePlus } from 'react-icons/ai';
 
 import { useQuery } from '@tanstack/react-query';
+import { Button, TextInput } from 'flowbite-react';
 import { useUtils } from '../../../contexts/UtilsProvider';
 import AddEmployeeModal from './Components/AddEmployeeModal';
 import DeleteEmployeeModal from './Components/DeleteEmployeeModal';
@@ -22,11 +23,14 @@ const Employees = () => {
   const [deleteEmployee, setDeleteEmployee] = useState([]);
   const [updateEmployee, setUpdateEmployee] = useState([]);
 
+  const [employeesData, setEmployeesData] = useState({});
+
   const { data: employees = [], refetch } = useQuery({
     queryKey: [],
     queryFn: async () => {
       const res = await fetch('http://localhost:5000/employees');
       const data = await res.json();
+      setEmployeesData(data);
       return data;
     },
   });
@@ -39,14 +43,40 @@ const Employees = () => {
   const { setDashboardTitle } = useUtils();
   setDashboardTitle('Employees');
 
+  const handleSearch = (e) => {
+    const searchString = e.target.searchString.value;
+    e.preventDefault();
+    const filterData = employeesData.filter((employeeData) =>
+      employeeData.fullName.toLowerCase().includes(searchString.toLowerCase())
+    );
+    setEmployeesData(filterData);
+  };
+
+  const handleClean = (e) => {
+    if (e.target.value === '') {
+      setEmployeesData(employees);
+    }
+  };
+
   return (
     <>
       <div>
-        <div className='flex justify-end mb-6'>
+        <div className='flex flex-col md:flex-row justify-between mb-6 gap-y-4'>
+          <form onSubmit={handleSearch} className='flex gap-4 w-full'>
+            <TextInput
+              onChange={handleClean}
+              name='searchString'
+              className='w-full md:!w-72'
+              type='text'
+              placeholder='Employee Name'
+            />
+            <Button type='submit'>Search</Button>
+          </form>
+
           <button
             onClick={addHandler}
             type='button'
-            className='flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-4 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+            className='shrink-0 flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-4 py-2.5  mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
           >
             <AiOutlinePlus className='mr-2 text-white' /> Add Employee
           </button>
@@ -75,7 +105,7 @@ const Employees = () => {
               </tr>
             </thead>
             <tbody>
-              {employees.map((employee) => (
+              {employeesData.map((employee) => (
                 <EmployeeRow
                   key={employee._id}
                   employee={employee}
