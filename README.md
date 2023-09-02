5b5cd6404a5d5e55487a605079021bb2b943d86f
Rubayet-billah <rubayetbillah909@gmail.com>
2023-09-02 20:19:25 +0600

    Readme edited

--- README.md
+++ README.md
@@ -1,13 +1,80 @@
-# Staff Deck
+# Staff Deck HR Management Software
 
-### Live: [Staff Deck](https://hr-management-client-2a430.web.app/)
+### Live Demo: [Staff Deck](https://hr-management-client-2a430.web.app/)
 
-[Server Repository](https://github.com/Rubayet-billah/hr-management-server)
+### Server Repository: [GitHub - Staff Deck Server](https://github.com/Rubayet-billah/hr-management-server)
 
-Testing email: admin@staff-deck.com <br />
-Testing password: 123123
+**Testing Credentials:**
 
-# Project Overview
+- Email: admin@staff-deck.com
+- Password: 123123
+
+## Project Overview
+
+Staff Deck is a comprehensive HR management software designed to streamline and automate various human resources processes. Developed by a team of dedicated contributors, the software offers a range of features to enhance efficiency and productivity in HR operations.
+
+## Key Features
+
+### 1. User-Friendly Landing Page
+
+- An engaging landing page introducing the software.
+- Sections highlighting main features, FAQs, and customer reviews.
+- Easy navigation and a slider for user testimonials.
+
+### 2. Job Application Management
+
+- A career route for job applicants.
+- Interactive multi-selection options for skills.
+- Efficient application submission and management.
+
+### 3. Dynamic Data Visualization
+
+- Real-time data visualization with human-readable charts.
+- Visual representations of company revenue, department metrics, and costs.
+- Responsive design for accessibility.
+
+### 4. Candidate Management
+
+- Comprehensive candidate listing with details, skills, and resumes.
+- Recruiters can easily decline or shortlist applications.
+- Integration with email for interview call notifications.
+
+### 5. Employee Management
+
+- Streamlined employee data management.
+- Add new employee details, including name, phone, designation, and salary.
+- Edit and search functionality for efficient data updates and retrieval.
+
+### 6. Payroll Management
+
+- Employee payroll details, including name, designation, salary, and absence tracking.
+- Automatic deduction of salary for absences.
+- Pay slip generation and PDF download for easy record-keeping.
+
+### 7. Admin Panel
+
+- Admin account management with the ability to add and delete administrators.
+- Enhanced security with Firebase integration.
+- User-friendly interface for admin tasks.
+
+### 8. Events and Holidays Tracking
+
+- Events section for tracking company events and scheduling.
+- Holidays page displaying upcoming holidays for planning purposes.
+
+### 9. Account Settings
+
+- Admins can update their information conveniently.
+- Improved account management and personalization options.
+
+<!-- ## Contributors
+
+- [Rubayet Billah](https://github.com/Rubayet-billah)
+- [Maruf Hossain](https://github.com/mickeymaruf)
+- [MD Badsha Fahadh](https://github.com/FahadMridha)
+- [Rajib Das](https://github.com/rajibdas-it)
+- [Tarekul Islam](https://github.com/itarek99)
+- [Naem Hossain](https://github.com/Naem-hossain) -->
 
 ## Contributors
 
--- src/Pages/Homepage/Components/DevsTeam.js
+++ src/Pages/Homepage/Components/DevsTeam.js
@@ -1,78 +1,78 @@
-import DevsCard from './DevsCard';
+import DevsCard from "./DevsCard";
 
-import maruf from '../../../assets/devs/Md-Maruf-Hossain.jpg';
-import badsha from '../../../assets/devs/Md.Badsha fahadh.jpg';
-import naem from '../../../assets/devs/naem.jpg';
-import rajib from '../../../assets/devs/rajib.jpeg';
-import rubayet from '../../../assets/devs/rubayet_billah.jpg';
-import tarek from '../../../assets/devs/tarekul_islam.jpg';
+import maruf from "../../../assets/devs/Md-Maruf-Hossain.jpg";
+import badsha from "../../../assets/devs/Md.Badsha fahadh.jpg";
+import naem from "../../../assets/devs/naem.jpg";
+import rajib from "../../../assets/devs/rajib.jpeg";
+import rubayet from "../../../assets/devs/rubayet_billah.jpg";
+import tarek from "../../../assets/devs/tarekul_islam.jpg";
 
 const devs = [
   {
     id: 1,
-    name: 'Tarekul Islam',
-    position: 'Full Stack Developer',
+    name: "Tarekul Islam",
+    position: "Full Stack Developer",
     image: tarek,
-    githubLink: '',
-    linkedInLink: '',
-    skills: ['html, css, javascript, react, node, mongoDB'],
+    githubLink: "",
+    linkedInLink: "",
+    skills: ["html, css, javascript, react, node, mongoDB"],
   },
   {
     id: 2,
-    name: 'Rubayet Billah',
-    position: 'Tech Lead',
+    name: "Rubayet Billah",
+    position: "Tech Lead",
     image: rubayet,
-    githubLink: '',
-    linkedInLink: '',
-    skills: ['html, css, javascript, react, node, mongoDB'],
+    githubLink: "https://github.com/Rubayet-billah",
+    linkedInLink: "https://www.linkedin.com/in/rubayet-billah/",
+    skills: ["html, css, javascript, react, node, mongoDB"],
   },
   {
     id: 3,
-    name: 'Naem Hossain',
-    position: 'MERN Stack Developer',
+    name: "Naem Hossain",
+    position: "MERN Stack Developer",
     image: naem,
-    githubLink: '',
-    linkedInLink: '',
-    skills: ['html, css, javascript, react, node, mongoDB'],
+    githubLink: "",
+    linkedInLink: "",
+    skills: ["html, css, javascript, react, node, mongoDB"],
   },
   {
     id: 4,
-    name: 'Md. Badsha Fahadh',
-    position: 'Full Stack Developer',
+    name: "Md. Badsha Fahadh",
+    position: "Full Stack Developer",
     image: badsha,
-    githubLink: '',
-    linkedInLink: '',
-    skills: ['html, css, javascript, react, node, mongoDB'],
+    githubLink: "",
+    linkedInLink: "",
+    skills: ["html, css, javascript, react, node, mongoDB"],
   },
   {
     id: 5,
-    name: 'Md Maruf Hossain',
-    position: 'Software Engineer',
+    name: "Md Maruf Hossain",
+    position: "Software Engineer",
     image: maruf,
-    githubLink: 'https://github.com/mickeymaruf',
-    linkedInLink: 'https://www.linkedin.com/in/mickeymaruf/',
-    skills: ['html, css, javascript, react, node, mongoDB'],
+    githubLink: "https://github.com/mickeymaruf",
+    linkedInLink: "https://www.linkedin.com/in/mickeymaruf/",
+    skills: ["html, css, javascript, react, node, mongoDB"],
   },
   {
     id: 6,
-    name: 'Rajib Das',
-    position: 'Back-End Developer',
+    name: "Rajib Das",
+    position: "Back-End Developer",
     image: rajib,
-    githubLink: '',
-    linkedInLink: '',
-    skills: ['html, css, javascript, react, node, mongoDB'],
+    githubLink: "",
+    linkedInLink: "",
+    skills: ["html, css, javascript, react, node, mongoDB"],
   },
 ];
 
 const DevsTeam = () => {
   return (
-    <div className='container mx-auto px-5 md:px-0 py-12'>
-      <div className='text-center mb-8'>
-        <h3 className='text-4xl font-bold mb-1'>Our Team</h3>
-        <p className='capitalize'>Let's meet out creative developer team.</p>
+    <div className="container mx-auto px-5 md:px-0 py-12">
+      <div className="text-center mb-8">
+        <h3 className="text-4xl font-bold mb-1">Our Team</h3>
+        <p className="capitalize">Let's meet out creative developer team.</p>
       </div>
 
-      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-12'>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-12">
         {devs.map((dev) => (
           <DevsCard key={dev.id} dev={dev} />
         ))}
